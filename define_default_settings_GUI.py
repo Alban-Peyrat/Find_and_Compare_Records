ba2bfc6cc505e679b490f45804002a3fb38ff7a0
Alban Peyrat (Archi) <alban.peyrat@paris-est.archi.fr>
2023-09-07 14:12:34 +0200

    Move exec specific settings to .env

--- define_default_settings_GUI.py
+++ define_default_settings_GUI.py
@@ -3,102 +3,102 @@
 # External import
 import PySimpleGUI as sg
 import json
+import os
+from dotenv import load_dotenv
+import dotenv
 
 # Internal import
 from theme.theme import *
+
+# Load env var (the settings)
+load_dotenv()
+DOTENV_FILE = dotenv.find_dotenv()
 
 # Get GUI parameters
 sg.set_options(font=font, icon=theme_name + "./theme/logo.ico", window_location=window_location)
 sg.theme_add_new(theme_name, theme)
 sg.theme(theme_name)
 
-# Load existing settings
-with open('./settings.json', "r+", encoding="utf-8") as f:
-    settings = json.load(f)
+# # --------------- The Layout ---------------
+layout = [
+    # Service name
+    [sg.Text("Nom du service :")],
+    [sg.Input(key="SERVICE", default_text=os.getenv("SERVICE"), size=(50, None))],
+    
+    # Original file path
+    [sg.Text("Fichier à analyser :")],
+    [sg.Input(key="FILE_PATH", default_text=os.getenv("FILE_PATH"), size=(80, None)), sg.FileBrowse()],
 
-    # # --------------- The Layout ---------------
-    layout = [
-        # Service name
-        [sg.Text("Nom du service :")],
-        [sg.Input(key="SERVICE", default_text=settings["SERVICE"], size=(50, None))],
-        
-        # Original file path
-        [sg.Text("Fichier à analyser :")],
-        [sg.Input(key="FILE_PATH", default_text=settings["FILE_PATH"], size=(80, None)), sg.FileBrowse()],
+    # Koha report number
+    [sg.Text("Rapport Koha :")],
+    [
+        sg.Text("Numéro de rapport :"),
+        sg.Input(key="KOHA_REPORT_NB", default_text=os.getenv("KOHA_REPORT_NB"), size=(6, None)),
+        sg.Text("Identifiant:"),
+        sg.Input(key="KOHA_USERID", default_text=os.getenv("KOHA_USERID"), size=(15, None)),
+        sg.Text("Mot de passe :"),
+        sg.Input(key="KOHA_PASSWORD", default_text=os.getenv("KOHA_PASSWORD"), size=(15, None), password_char="*"),
+    ],
 
-        # Koha report number
-        [sg.Text("Rapport Koha :")],
-        [
-            sg.Text("Numéro de rapport :"),
-            sg.Input(key="KOHA_REPORT_NB", default_text=settings["KOHA_REPORT_NB"], size=(6, None)),
-            sg.Text("Identifiant:"),
-            sg.Input(key="KOHA_USERID", default_text=settings["KOHA_USERID"], size=(15, None)),
-            sg.Text("Mot de passe :"),
-            sg.Input(key="KOHA_PASSWORD", default_text=settings["KOHA_PASSWORD"], size=(15, None), password_char="*"),
-        ],
+    # Output folder
+    [sg.Text("Dossier contenant les résultats :")],
+    [sg.Input(key="OUTPUT_PATH", default_text=os.getenv("OUTPUT_PATH"), size=(80, None)), sg.FolderBrowse()],
 
-        # Output folder
-        [sg.Text("Dossier contenant les résultats :")],
-        [sg.Input(key="OUTPUT_PATH", default_text=settings["OUTPUT_PATH"], size=(80, None)), sg.FolderBrowse()],
+    # Logs path
+    [sg.Text("Dossier contenant les logs :")],
+    [sg.Input(key="LOGS_PATH", default_text=os.getenv("LOGS_PATH"), size=(80, None)), sg.FolderBrowse()],
 
-        # Logs path
-        [sg.Text("Dossier contenant les logs :")],
-        [sg.Input(key="LOGS_PATH", default_text=settings["LOGS_PATH"], size=(80, None)), sg.FolderBrowse()],
+    # Koha URL
+    [
+        sg.Text("Koha URL :"),
+        sg.Input(key="KOHA_URL", default_text=os.getenv("KOHA_URL"), size=(60, None))
+    ],
 
-        # Koha URL
-        [
-            sg.Text("Koha URL :"),
-            sg.Input(key="KOHA_URL", default_text=settings["KOHA_URL"], size=(60, None))
-        ],
+    # Koha PPN + ILN + RCR
+    [
+        sg.Text("Koha champ PPN :"),
+        sg.Input(key="KOHA_PPN_FIELD", default_text=os.getenv("KOHA_PPN_FIELD"), size=(3, None)),
+        sg.Text("Koha sous-champ PPN :"),
+        sg.Input(key="KOHA_PPN_SUBFIELD", default_text=os.getenv("KOHA_PPN_SUBFIELD"), size=(1, None)),
+        sg.Text("ILN :"),
+        sg.Input(key="ILN", default_text=os.getenv("ILN"), size=(3, None)),
+        sg.Text("RCR :"),
+        sg.Input(key="RCR", default_text=os.getenv("RCR"), size=(9, None))
+    ],
 
-        # Koha PPN + ILN + RCR
-        [
-            sg.Text("Koha champ PPN :"),
-            sg.Input(key="KOHA_PPN_FIELD", default_text=settings["KOHA_PPN_FIELD"], size=(3, None)),
-            sg.Text("Koha sous-champ PPN :"),
-            sg.Input(key="KOHA_PPN_SUBFIELD", default_text=settings["KOHA_PPN_SUBFIELD"], size=(1, None)),
-            sg.Text("ILN :"),
-            sg.Input(key="ILN", default_text=settings["ILN"], size=(3, None)),
-            sg.Text("RCR :"),
-            sg.Input(key="RCR", default_text=settings["RCR"], size=(9, None))
-        ],
+    # Submit
+    [sg.Button('Sauvegarder les paramètres par défaut', key="submit")]
+]
 
-        # Submit
-        [sg.Button('Sauvegarder les paramètres par défaut', key="submit")]
-    ]
+# # --------------- Window Definition ---------------
+# # Create the window
+window = sg.Window("Paramétrer les valeurs par défaut de Compare Koha Sudoc records", layout)
 
-    # # --------------- Window Definition ---------------
-    # # Create the window
-    window = sg.Window("Paramétrer les valeurs par défaut de Compare Koha Sudoc records", layout)
-
-    # # --------------- Event loop or Window.read call ---------------
-    # # Display and interact with the Window
-    # event, values = window.read()
-    event, val = window.read()
+# # --------------- Event loop or Window.read call ---------------
+# # Display and interact with the Window
+# event, values = window.read()
+event, val = window.read()
 
 
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        print("Application quittée par l'usager")
-        exit()
+if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    print("Application quittée par l'usager")
+    exit()
 
-    # Rewrite settings
-    settings["SERVICE"] = val["SERVICE"]
-    settings["FILE_PATH"] = val["FILE_PATH"]
-    settings["KOHA_REPORT_NB"] = val["KOHA_REPORT_NB"]
-    settings["KOHA_USERID"] = val["KOHA_USERID"]
-    settings["KOHA_PASSWORD"] = val["KOHA_PASSWORD"]
-    settings["OUTPUT_PATH"] = val["OUTPUT_PATH"]
-    settings["LOGS_PATH"] = val["LOGS_PATH"]
-    settings["KOHA_URL"] = val["KOHA_URL"]
-    settings["KOHA_PPN_FIELD"] = val["KOHA_PPN_FIELD"]
-    settings["KOHA_PPN_SUBFIELD"] = val["KOHA_PPN_SUBFIELD"]
-    settings["ILN"] = val["ILN"]
-    settings["RCR"] = val["RCR"]
+# Rewrite settings
+dotenv.set_key(DOTENV_FILE, "SERVICE", val["SERVICE"])
+dotenv.set_key(DOTENV_FILE, "FILE_PATH", val["FILE_PATH"])
+dotenv.set_key(DOTENV_FILE, "KOHA_REPORT_NB", val["KOHA_REPORT_NB"])
+dotenv.set_key(DOTENV_FILE, "KOHA_USERID", val["KOHA_USERID"])
+dotenv.set_key(DOTENV_FILE, "KOHA_PASSWORD", val["KOHA_PASSWORD"])
+dotenv.set_key(DOTENV_FILE, "OUTPUT_PATH", val["OUTPUT_PATH"])
+dotenv.set_key(DOTENV_FILE, "LOGS_PATH", val["LOGS_PATH"])
+dotenv.set_key(DOTENV_FILE, "KOHA_URL", val["KOHA_URL"])
+dotenv.set_key(DOTENV_FILE, "KOHA_PPN_FIELD", val["KOHA_PPN_FIELD"])
+dotenv.set_key(DOTENV_FILE, "KOHA_PPN_SUBFIELD", val["KOHA_PPN_SUBFIELD"])
+dotenv.set_key(DOTENV_FILE, "ILN", val["ILN"])
+dotenv.set_key(DOTENV_FILE, "RCR", val["RCR"])
 
-    f.seek(0)
-    json.dump(settings, f, indent=4)
-    f.truncate()
-    print("Paramètres par défaut sauvegardés avec succès")
+print("Paramètres par défaut sauvegardés avec succès")
 
-    # # --------------- Closing the window ---------------
-    window.close()
+# # --------------- Closing the window ---------------
+window.close()
--- main_gui.py
+++ main_gui.py
@@ -3,10 +3,15 @@
 # External import
 import PySimpleGUI as sg
 import json
+import os
+from dotenv import load_dotenv
 
 # Internal import
 from theme.theme import *
 import main
+
+# Load env var
+load_dotenv()
 
 # Get GUI parameters
 sg.set_options(font=font, icon=theme_name + "./theme/logo.ico", window_location=window_location)
@@ -14,97 +19,98 @@ sg.theme_add_new(theme_name, theme)
 sg.theme(theme_name)
 
 # Load existing settings
+settings = {}
 with open('./settings.json', "r+", encoding="utf-8") as f:
     settings = json.load(f)
 
-    # --------------- The Layout ---------------
-    layout = [
-        # Service name
-        [sg.Text("Nom du service :")],
-        [sg.Input(key="SERVICE", default_text=settings["SERVICE"], size=(50, None))],
-        
-        # Data source
-        [
-            sg.Text("Source de données :"),
-            sg.Radio("Fichier", "DATA_SOURCE", default=True, size=(7,1), key='DATA-SOURCE-FILE'),
-            sg.Radio("Rapport Koha", "DATA_SOURCE", default=False, size=(12,1), key="DATA-SOURCE-KOHA-REPORT")
-        ],
+# --------------- The Layout ---------------
+layout = [
+    # Service name
+    [sg.Text("Nom du service :")],
+    [sg.Input(key="SERVICE", default_text=os.getenv("SERVICE"), size=(50, None))],
+    
+    # Data source
+    [
+        sg.Text("Source de données :"),
+        sg.Radio("Fichier", "DATA_SOURCE", default=True, size=(7,1), key='DATA-SOURCE-FILE'),
+        sg.Radio("Rapport Koha", "DATA_SOURCE", default=False, size=(12,1), key="DATA-SOURCE-KOHA-REPORT")
+    ],
 
-        # Original file path
-        [sg.Text("Fichier à analyser :")],
-        [sg.Input(key="FILE_PATH", default_text=settings["FILE_PATH"], size=(80, None)), sg.FileBrowse()],
+    # Original file path
+    [sg.Text("Fichier à analyser :")],
+    [sg.Input(key="FILE_PATH", default_text=os.getenv("FILE_PATH"), size=(80, None)), sg.FileBrowse()],
 
-        # Koha report number
-        [sg.Text("Si utilisation d'un rapport Koha :")],
-        [
-            sg.Text("Numéro de rapport :"),
-            sg.Input(key="KOHA_REPORT_NB", default_text=settings["KOHA_REPORT_NB"], size=(6, None)),
-            sg.Text("Identifiant:"),
-            sg.Input(key="KOHA_USERID", default_text=settings["KOHA_USERID"], size=(15, None)),
-            sg.Text("Mot de passe :"),
-            sg.Input(key="KOHA_PASSWORD", default_text=settings["KOHA_PASSWORD"], size=(15, None), password_char="*"),
-        ],
+    # Koha report number
+    [sg.Text("Si utilisation d'un rapport Koha :")],
+    [
+        sg.Text("Numéro de rapport :"),
+        sg.Input(key="KOHA_REPORT_NB", default_text=os.getenv("KOHA_REPORT_NB"), size=(6, None)),
+        sg.Text("Identifiant:"),
+        sg.Input(key="KOHA_USERID", default_text=os.getenv("KOHA_USERID"), size=(15, None)),
+        sg.Text("Mot de passe :"),
+        sg.Input(key="KOHA_PASSWORD", default_text=os.getenv("KOHA_PASSWORD"), size=(15, None), password_char="*"),
+    ],
 
-        # Output folder
-        [sg.Text("Dossier contenant les résultats :")],
-        [sg.Input(key="OUTPUT_PATH", default_text=settings["OUTPUT_PATH"], size=(80, None)), sg.FolderBrowse()],
+    # Output folder
+    [sg.Text("Dossier contenant les résultats :")],
+    [sg.Input(key="OUTPUT_PATH", default_text=os.getenv("OUTPUT_PATH"), size=(80, None)), sg.FolderBrowse()],
 
-        # Logs path
-        [sg.Text("Dossier contenant les logs :")],
-        [sg.Input(key="LOGS_PATH", default_text=settings["LOGS_PATH"], size=(80, None)), sg.FolderBrowse()],
+    # Logs path
+    [sg.Text("Dossier contenant les logs :")],
+    [sg.Input(key="LOGS_PATH", default_text=os.getenv("LOGS_PATH"), size=(80, None)), sg.FolderBrowse()],
 
-        # Koha URL
-        [
-            sg.Text("Koha URL :"),
-            sg.Input(key="KOHA_URL", default_text=settings["KOHA_URL"], size=(60, None))
-        ],
+    # Koha URL
+    [
+        sg.Text("Koha URL :"),
+        sg.Input(key="KOHA_URL", default_text=os.getenv("KOHA_URL"), size=(60, None))
+    ],
 
-        # Koha PPN + ILN + RCR
-        [
-            sg.Text("Koha champ PPN :"),
-            sg.Input(key="KOHA_PPN_FIELD", default_text=settings["KOHA_PPN_FIELD"], size=(3, None)),
-            sg.Text("Koha sous-champ PPN :"),
-            sg.Input(key="KOHA_PPN_SUBFIELD", default_text=settings["KOHA_PPN_SUBFIELD"], size=(1, None)),
-            sg.Text("ILN :"),
-            sg.Input(key="ILN", default_text=settings["ILN"], size=(3, None)),
-            sg.Text("RCR :"),
-            sg.Input(key="RCR", default_text=settings["RCR"], size=(9, None))
-        ],
+    # Koha PPN + ILN + RCR
+    [
+        sg.Text("Koha champ PPN :"),
+        sg.Input(key="KOHA_PPN_FIELD", default_text=os.getenv("KOHA_PPN_FIELD"), size=(3, None)),
+        sg.Text("Koha sous-champ PPN :"),
+        sg.Input(key="KOHA_PPN_SUBFIELD", default_text=os.getenv("KOHA_PPN_SUBFIELD"), size=(1, None)),
+        sg.Text("ILN :"),
+        sg.Input(key="ILN", default_text=os.getenv("ILN"), size=(3, None)),
+        sg.Text("RCR :"),
+        sg.Input(key="RCR", default_text=os.getenv("RCR"), size=(9, None))
+    ],
 
-        # Submit
-        [sg.Button("Lancer l'analyse", key="submit")]
-    ]
+    # Submit
+    [sg.Button("Lancer l'analyse", key="submit")]
+]
 
-    # # --------------- Window Definition ---------------
-    # # Create the window
-    window = sg.Window("Paramétrer les valeurs par défaut de Compare Koha Sudoc records", layout)
+# # --------------- Window Definition ---------------
+# # Create the window
+window = sg.Window("Paramétrer les valeurs par défaut de Compare Koha Sudoc records", layout)
 
-    # # --------------- Event loop or Window.read call ---------------
-    # # Display and interact with the Window
-    # event, values = window.read()
-    event, val = window.read()
+# # --------------- Event loop or Window.read call ---------------
+# # Display and interact with the Window
+# event, values = window.read()
+event, val = window.read()
 
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        print("Application quittée par l'usager")
-        exit()
+if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    print("Application quittée par l'usager")
+    exit()
 
-    # # --------------- Closing the window ---------------
-    window.close()
+# # --------------- Closing the window ---------------
+window.close()
 
-    # Launch the main script
-    print("Exécution du script principal")
-    main.main(SERVICE = val["SERVICE"],
-        FILE_PATH = val["FILE_PATH"],
-        OUTPUT_PATH = val["OUTPUT_PATH"],
-        LOGS_PATH = val["LOGS_PATH"],
-        ANALYSIS = settings["ANALYSIS"],
-        CSV_EXPORT_COLS = settings["CSV_EXPORT_COLS"],
-        REPORT_SETTINGS = settings["REPORT_SETTINGS"],
-        KOHA_URL = val["KOHA_URL"],
-        KOHA_PPN_FIELD = val["KOHA_PPN_FIELD"],
-        KOHA_PPN_SUBFIELD = val["KOHA_PPN_SUBFIELD"],
-        KOHA_REPORT_NB = val["KOHA_REPORT_NB"],
-        KOHA_USERID = val["KOHA_USERID"],
-        KOHA_PASSWORD = val["KOHA_PASSWORD"],
-        ILN = val["ILN"],
-        RCR = val["RCR"])
+# Launch the main script
+print("Exécution du script principal")
+main.main(SERVICE = val["SERVICE"],
+    FILE_PATH = val["FILE_PATH"],
+    OUTPUT_PATH = val["OUTPUT_PATH"],
+    LOGS_PATH = val["LOGS_PATH"],
+    ANALYSIS = settings["ANALYSIS"],
+    CSV_EXPORT_COLS = settings["CSV_EXPORT_COLS"],
+    REPORT_SETTINGS = settings["REPORT_SETTINGS"],
+    KOHA_URL = val["KOHA_URL"],
+    KOHA_PPN_FIELD = val["KOHA_PPN_FIELD"],
+    KOHA_PPN_SUBFIELD = val["KOHA_PPN_SUBFIELD"],
+    KOHA_REPORT_NB = val["KOHA_REPORT_NB"],
+    KOHA_USERID = val["KOHA_USERID"],
+    KOHA_PASSWORD = val["KOHA_PASSWORD"],
+    ILN = val["ILN"],
+    RCR = val["RCR"])
