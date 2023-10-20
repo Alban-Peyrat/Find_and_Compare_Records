659be3897b7748b64ddceadafaf3b9ed2784fc80
Alban Peyrat (Archi) <alban.peyrat@paris-est.archi.fr>
2023-10-20 16:11:00 +0200

    AR388 : update colors

--- theme/theme.py
+++ theme/theme.py
@@ -1,20 +1,23 @@
 # https://github.com/PySimpleGUI/PySimpleGUI/tree/master/ThemeMaker
+from enum import Enum
+
+class ArchiRes_Colors(Enum):
+    RED = "#d6441c"
+    GREY = "#575551"
+    LIGHT_GREY = "#efefef"
+    BLACK_FONTS = "#001111"
+    WHITE = "#ffffff"
+
 
 theme_name = "ArchiRes_Theme"
-theme = {'BACKGROUND': '#121212',
-             'TEXT': '#EBE0EB',
-             'INPUT': '#9FDEFF',#changer
-             'TEXT_INPUT': '#121212',
-             'SCROLL': '#505F69',
-             'BUTTON': ('#EBE0EB', '#660000'),
-             'PROGRESS': ('#121212', '#9FDEFF'),
+theme = {'BACKGROUND': ArchiRes_Colors.GREY.value,
+             'TEXT': ArchiRes_Colors.WHITE.value,
+             'INPUT': ArchiRes_Colors.LIGHT_GREY.value,
+             'TEXT_INPUT': ArchiRes_Colors.BLACK_FONTS.value,
+             'SCROLL': ArchiRes_Colors.BLACK_FONTS.value,
+             'BUTTON': (ArchiRes_Colors.WHITE.value, ArchiRes_Colors.RED.value),
+             'PROGRESS': (ArchiRes_Colors.WHITE.value, ArchiRes_Colors.GREY.value),
              'BORDER': 1, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0
              }
 font = ("Verdana", 16)
 window_location = (0, 0)
-
-# Couleurs
-# Le rouge : #660000
-# Le noir : #121212
-# Le bleu : #9FDEFF
-# Le blanc : #EBE0EB
